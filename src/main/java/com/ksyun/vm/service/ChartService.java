8869b8074315dc3bb922aaa052825bf57cfe64de
yuri <liuyu1@kingsoft.com>
2013-12-23 16:10:33 +0800

    增加环境区分代码

--- src/main/java/com/ksyun/vm/service/ChartService.java
+++ src/main/java/com/ksyun/vm/service/ChartService.java
@@ -18,6 +18,7 @@ import com.ksyun.vm.pojo.chart.MonitorVmDiskPo;
 import com.ksyun.vm.pojo.chart.MonitorVmLoadPo;
 import com.ksyun.vm.pojo.chart.MonitorVmNetworkPo;
 import com.ksyun.vm.pojo.chart.MonitorVmStatusFlowPo;
+import com.ksyun.vm.utils.Constants;
 import com.ksyun.vm.utils.InitConst;
 import com.ksyun.vm.utils.Tools;
 
@@ -32,16 +33,25 @@ public class ChartService {
     @Autowired
     private HBaseDao hbaseDao;
     
+    
+	String status_table_name=InitConst.MONITOR_HBASE_TABLE_STATUS+"_"+Constants.getPropertyValue("sys.type");
+	String load_table_name=InitConst.MONITOR_HBASE_TABLE_LOAD+"_"+Constants.getPropertyValue("sys.type");
+	String network_table_name=InitConst.MONITOR_HBASE_TABLE_NETWORK+"_"+Constants.getPropertyValue("sys.type");
+	String disk_table_name=InitConst.MONITOR_HBASE_TABLE_DISK+"_"+Constants.getPropertyValue("sys.type");
+	
+	
     public MonitorVmLoadPo getLoad(String vmuuid){
     	Calendar c = Calendar.getInstance();
         c.add(Calendar.SECOND,-30);
         String startTime = String.valueOf(c.getTimeInMillis()/1000);
         String endTime = String.valueOf(new Date().getTime()/1000);
-        List<Map<String, HBaseCell>> result = hbaseDao.scaner(InitConst.MONITOR_HBASE_TABLE_LOAD, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
+        List<Map<String, HBaseCell>> result = hbaseDao.scaner(load_table_name, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
         MonitorVmLoadPo po = null;
-        for(Map<String, HBaseCell> row : result){
-        	HBaseCell cell = row.get("load");
-        	po = (MonitorVmLoadPo)JSONObject.parseObject(cell.getValue(),MonitorVmLoadPo.class);
+        if(result != null){
+        	for(Map<String, HBaseCell> row : result){
+            	HBaseCell cell = row.get("load");
+            	po = (MonitorVmLoadPo)JSONObject.parseObject(cell.getValue(),MonitorVmLoadPo.class);
+            }
         }
         return po;
     }
@@ -51,24 +61,27 @@ public class ChartService {
         c.add(Calendar.SECOND,-30);
         String startTime = String.valueOf(c.getTimeInMillis()/1000);
         String endTime = String.valueOf(new Date().getTime()/1000);
-        List<Map<String, HBaseCell>> result = hbaseDao.scaner(InitConst.MONITOR_HBASE_TABLE_DISK, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
+        List<Map<String, HBaseCell>> result = hbaseDao.scaner(disk_table_name, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
         List<MonitorVmDiskPo> list = new ArrayList<MonitorVmDiskPo>();
-        for(Map<String, HBaseCell> row : result){
-        	Set<Map.Entry<String, HBaseCell>> set = row.entrySet();
-            for (Iterator<Map.Entry<String, HBaseCell>> it = set.iterator(); it.hasNext();) {
-                Map.Entry<String, HBaseCell> entry = (Map.Entry<String, HBaseCell>) it.next();
-                MonitorVmDiskPo po = (MonitorVmDiskPo)JSONObject.parseObject(entry.getValue().getValue(),MonitorVmDiskPo.class);
-                list.add(po);
+        if(result != null){
+        	for(Map<String, HBaseCell> row : result){
+            	Set<Map.Entry<String, HBaseCell>> set = row.entrySet();
+                for (Iterator<Map.Entry<String, HBaseCell>> it = set.iterator(); it.hasNext();) {
+                    Map.Entry<String, HBaseCell> entry = (Map.Entry<String, HBaseCell>) it.next();
+                    MonitorVmDiskPo po = (MonitorVmDiskPo)JSONObject.parseObject(entry.getValue().getValue(),MonitorVmDiskPo.class);
+                    list.add(po);
+                }
             }
+            System.out.println(list);
+            MonitorVmDiskPo resultPo = new MonitorVmDiskPo();
+            for(MonitorVmDiskPo po : list){
+            	if(po.getDisk().equals(disk)){
+            		resultPo = po;
+            	}
+            }
+            return resultPo;
         }
-        System.out.println(list);
-        MonitorVmDiskPo resultPo = new MonitorVmDiskPo();
-        for(MonitorVmDiskPo po : list){
-        	if(po.getDisk().equals(disk)){
-        		resultPo = po;
-        	}
-        }
-        return resultPo;
+        return null;
     }
 
     public MonitorVmNetworkPo getNetwork(String vmuuid,String mac){
@@ -76,24 +89,27 @@ public class ChartService {
         c.add(Calendar.SECOND,-30);
         String startTime = String.valueOf(c.getTimeInMillis()/1000);
         String endTime = String.valueOf(new Date().getTime()/1000);
-        List<Map<String, HBaseCell>> result = hbaseDao.scaner(InitConst.MONITOR_HBASE_TABLE_NETWORK, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
+        List<Map<String, HBaseCell>> result = hbaseDao.scaner(network_table_name, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
         List<MonitorVmNetworkPo> list = new ArrayList<MonitorVmNetworkPo>();
-        for(Map<String, HBaseCell> row : result){
-        	Set<Map.Entry<String, HBaseCell>> set = row.entrySet();
-            for (Iterator<Map.Entry<String, HBaseCell>> it = set.iterator(); it.hasNext();) {
-                Map.Entry<String, HBaseCell> entry = (Map.Entry<String, HBaseCell>) it.next();
-                MonitorVmNetworkPo po = (MonitorVmNetworkPo)JSONObject.parseObject(entry.getValue().getValue(),MonitorVmNetworkPo.class);
-                list.add(po);
+        if(result != null){
+        	for(Map<String, HBaseCell> row : result){
+            	Set<Map.Entry<String, HBaseCell>> set = row.entrySet();
+                for (Iterator<Map.Entry<String, HBaseCell>> it = set.iterator(); it.hasNext();) {
+                    Map.Entry<String, HBaseCell> entry = (Map.Entry<String, HBaseCell>) it.next();
+                    MonitorVmNetworkPo po = (MonitorVmNetworkPo)JSONObject.parseObject(entry.getValue().getValue(),MonitorVmNetworkPo.class);
+                    list.add(po);
+                }
             }
+            System.out.println(list);
+            MonitorVmNetworkPo resultPo = new MonitorVmNetworkPo();
+            for(MonitorVmNetworkPo po : list){
+            	if(po.getMac().equals(mac)){
+            		resultPo = po;
+            	}
+            }
+            return resultPo;
         }
-        System.out.println(list);
-        MonitorVmNetworkPo resultPo = new MonitorVmNetworkPo();
-        for(MonitorVmNetworkPo po : list){
-        	if(po.getMac().equals(mac)){
-        		resultPo = po;
-        	}
-        }
-        return resultPo;
+        return null;
     }
 
     public MonitorVmStatusFlowPo getStatus(String vmuuid){
@@ -101,12 +117,15 @@ public class ChartService {
         c.add(Calendar.SECOND,-30);
         String startTime = String.valueOf(c.getTimeInMillis()/1000);
         String endTime = String.valueOf(new Date().getTime()/1000);
-        List<Map<String, HBaseCell>> result = hbaseDao.scaner(InitConst.MONITOR_HBASE_TABLE_STATUS, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
+        List<Map<String, HBaseCell>> result = hbaseDao.scaner(status_table_name, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
         MonitorVmStatusFlowPo po = null;
-        for(Map<String, HBaseCell> row : result){
-        	HBaseCell cell = row.get("load");
-        	po = (MonitorVmStatusFlowPo)JSONObject.parseObject(cell.getValue(),MonitorVmStatusFlowPo.class);
+        if(result != null){
+	    	 for(Map<String, HBaseCell> row : result){
+	         	HBaseCell cell = row.get("load");
+	         	po = (MonitorVmStatusFlowPo)JSONObject.parseObject(cell.getValue(),MonitorVmStatusFlowPo.class);
+	         }
         }
+       
         return po;
     }
 
@@ -115,15 +134,18 @@ public class ChartService {
         c.add(Calendar.DATE,-1);
         String startTime = String.valueOf(c.getTimeInMillis()/1000);
         String endTime = String.valueOf(new Date().getTime()/1000);
-        List<Map<String, HBaseCell>> result = hbaseDao.scaner(InitConst.MONITOR_HBASE_TABLE_LOAD, Tools.makeRowKey(vmuuid, startTime), Tools.makeRowKey(vmuuid, endTime));
+        List<Map<String, HBaseCell>> result = hbaseDao.scaner(load_table_name, Tools.makeRowKey(vmuuid, startTime), Tools.makeRowKey(vmuuid, endTime));
         List<MonitorVmLoadPo> poList = new ArrayList<MonitorVmLoadPo>();
-        for(Map<String, HBaseCell> row : result){
-        	HBaseCell cell = row.get("load");
-        	MonitorVmLoadPo po = (MonitorVmLoadPo)JSONObject.parseObject(cell.getValue(),MonitorVmLoadPo.class);
-            poList.add(po);
+        if(result != null){
+        	for(Map<String, HBaseCell> row : result){
+            	HBaseCell cell = row.get("load");
+            	MonitorVmLoadPo po = (MonitorVmLoadPo)JSONObject.parseObject(cell.getValue(),MonitorVmLoadPo.class);
+                poList.add(po);
+            }
+            System.out.println(poList);
+            return poList;
         }
-        System.out.println(poList);
-        return poList;
+        return null;
     }
 
     public List<MonitorVmNetworkPo> getNetworkOneDaysAgo(String vmuuid) {
@@ -131,18 +153,21 @@ public class ChartService {
     	c.add(Calendar.DATE,-1);
     	String startTime = String.valueOf(c.getTimeInMillis()/1000);
         String endTime = String.valueOf(new Date().getTime()/1000);
-        List<Map<String, HBaseCell>> result = hbaseDao.scaner(InitConst.MONITOR_HBASE_TABLE_NETWORK, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
+        List<Map<String, HBaseCell>> result = hbaseDao.scaner(network_table_name, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
         List<MonitorVmNetworkPo> poList = new ArrayList<MonitorVmNetworkPo>();
-        for(Map<String, HBaseCell> row : result){
-        	Set<Map.Entry<String, HBaseCell>> set = row.entrySet();
-            for (Iterator<Map.Entry<String, HBaseCell>> it = set.iterator(); it.hasNext();) {
-                Map.Entry<String, HBaseCell> entry = (Map.Entry<String, HBaseCell>) it.next();
-                MonitorVmNetworkPo po = (MonitorVmNetworkPo)JSONObject.parseObject(entry.getValue().getValue(),MonitorVmNetworkPo.class);
-                poList.add(po);
+        if(result != null){
+        	for(Map<String, HBaseCell> row : result){
+            	Set<Map.Entry<String, HBaseCell>> set = row.entrySet();
+                for (Iterator<Map.Entry<String, HBaseCell>> it = set.iterator(); it.hasNext();) {
+                    Map.Entry<String, HBaseCell> entry = (Map.Entry<String, HBaseCell>) it.next();
+                    MonitorVmNetworkPo po = (MonitorVmNetworkPo)JSONObject.parseObject(entry.getValue().getValue(),MonitorVmNetworkPo.class);
+                    poList.add(po);
+                }
             }
+            System.out.println(poList);
+            return poList;
         }
-        System.out.println(poList);
-        return poList;
+        return null;
     }
 
     public List<MonitorVmDiskPo> getDiskOneDaysAgo(String vmuuid) {
@@ -150,18 +175,21 @@ public class ChartService {
     	c.add(Calendar.DATE,-1);
     	String startTime = String.valueOf(c.getTimeInMillis()/1000);
         String endTime = String.valueOf(new Date().getTime()/1000);
-        List<Map<String, HBaseCell>> result = hbaseDao.scaner(InitConst.MONITOR_HBASE_TABLE_DISK, Tools.makeRowKey(vmuuid, startTime), Tools.makeRowKey(vmuuid,endTime));
+        List<Map<String, HBaseCell>> result = hbaseDao.scaner(disk_table_name, Tools.makeRowKey(vmuuid, startTime), Tools.makeRowKey(vmuuid,endTime));
         List<MonitorVmDiskPo> poList = new ArrayList<MonitorVmDiskPo>();
-        for(Map<String, HBaseCell> row : result){
-        	Set<Map.Entry<String, HBaseCell>> set = row.entrySet();
-            for (Iterator<Map.Entry<String, HBaseCell>> it = set.iterator(); it.hasNext();) {
-                Map.Entry<String, HBaseCell> entry = (Map.Entry<String, HBaseCell>) it.next();
-                MonitorVmDiskPo po = (MonitorVmDiskPo)JSONObject.parseObject(entry.getValue().getValue(),MonitorVmDiskPo.class);
-                poList.add(po);
+        if(result != null){
+        	for(Map<String, HBaseCell> row : result){
+            	Set<Map.Entry<String, HBaseCell>> set = row.entrySet();
+                for (Iterator<Map.Entry<String, HBaseCell>> it = set.iterator(); it.hasNext();) {
+                    Map.Entry<String, HBaseCell> entry = (Map.Entry<String, HBaseCell>) it.next();
+                    MonitorVmDiskPo po = (MonitorVmDiskPo)JSONObject.parseObject(entry.getValue().getValue(),MonitorVmDiskPo.class);
+                    poList.add(po);
+                }
             }
+            System.out.println(poList);
+            return poList;
         }
-        System.out.println(poList);
-        return poList;
+        return null;
     }
 
     public List<MonitorVmStatusFlowPo> getStatusOneDaysAgo(String vmuuid) {
@@ -169,15 +197,18 @@ public class ChartService {
     	c.add(Calendar.DATE,-1);
     	String startTime = String.valueOf(c.getTimeInMillis()/1000);
         String endTime = String.valueOf(new Date().getTime()/1000);
-        List<Map<String, HBaseCell>> result = hbaseDao.scaner(InitConst.MONITOR_HBASE_TABLE_STATUS, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
+        List<Map<String, HBaseCell>> result = hbaseDao.scaner(status_table_name, Tools.makeRowKey(vmuuid, startTime),Tools.makeRowKey(vmuuid, endTime));
         List<MonitorVmStatusFlowPo> poList = new ArrayList<MonitorVmStatusFlowPo>();
-        for(Map<String, HBaseCell> row : result){
-        	HBaseCell cell = row.get("status");
-        	MonitorVmStatusFlowPo po = (MonitorVmStatusFlowPo)JSONObject.parseObject(cell.getValue(),MonitorVmStatusFlowPo.class);
-            poList.add(po);
+        if(result != null){
+        	for(Map<String, HBaseCell> row : result){
+            	HBaseCell cell = row.get("status");
+            	MonitorVmStatusFlowPo po = (MonitorVmStatusFlowPo)JSONObject.parseObject(cell.getValue(),MonitorVmStatusFlowPo.class);
+                poList.add(po);
+            }
+            System.out.println(poList);
+            return poList;
         }
-        System.out.println(poList);
-        return poList;
+        return null;
     }
     public static void main(String[] args) {
     	Calendar c = Calendar.getInstance();
--- src/main/java/com/ksyun/vm/utils/InitConst.java
+++ src/main/java/com/ksyun/vm/utils/InitConst.java
@@ -63,8 +63,8 @@ public class InitConst {
 
     public static final String KVM_VNC = "kvm.vnc";
     
-    public static final String MONITOR_HBASE_TABLE_STATUS="vm_table_status_test";
-    public static final String MONITOR_HBASE_TABLE_NETWORK="vm_table_network_test";
-    public static final String MONITOR_HBASE_TABLE_LOAD="vm_table_load_test";
-    public static final String MONITOR_HBASE_TABLE_DISK="vm_table_disk_test";
+    public static final String MONITOR_HBASE_TABLE_STATUS="vm_table_status";
+    public static final String MONITOR_HBASE_TABLE_NETWORK="vm_table_network";
+    public static final String MONITOR_HBASE_TABLE_LOAD="vm_table_load";
+    public static final String MONITOR_HBASE_TABLE_DISK="vm_table_disk";
 }
