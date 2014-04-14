4cf5a550c5aa8b1ef939246e59ca3950f6f6ef75
xueqi <xueqi@kingsoft.com>
2014-04-14 14:05:56 +0800

    权限控制，过滤 退出

--- src/main/java/com/ksyun/vm/interceptor/HandleAuthenticationInterceptor.java
+++ src/main/java/com/ksyun/vm/interceptor/HandleAuthenticationInterceptor.java
@@ -49,7 +49,8 @@ public class HandleAuthenticationInterceptor extends HandlerInterceptorAdapter {
 		 */
 		if (uri.indexOf("/login") > -1 || uri.indexOf("/html") > -1
 				|| uri.indexOf("/js") > -1 || uri.indexOf("/img") > -1
-				|| uri.indexOf("/getvmsum") > -1 || uri.indexOf("/change") > -1) {
+				|| uri.indexOf("/getvmsum") > -1 || uri.indexOf("/change") > -1
+				|| uri.indexOf("/logout") > -1) {
 			// 过滤对change连接的权限控制
 			return true;
 		}
